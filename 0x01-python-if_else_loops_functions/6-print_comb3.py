f9c29aba740b8ec10f3bfca301e4d0e942776cb0
lauraliliana <lauraliliana@gmail.com>
2020-12-08 23:48:19 -0500

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(1, 90):
+        if i < 10:
+                    print("{:02d}, ".format(i), end="")
+                        if i > 9 and i < 89 and i / 10 != i % 10 and i / 10 < i % 10:
+                                    print("{}, ".format(i), end="")
+                                        if i == 89:
+                                                    print("{}".format(i))
