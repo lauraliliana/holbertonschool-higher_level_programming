b92020a59abc5e6317e90abfe8cfea770d6404ef
lauraliliana <lauraliliana@gmail.com>
2020-12-10 22:27:48 -0500

    task 2, this is the one

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+if __name__ == "__main__":
+    n = len(sys.argv)
+    if n - 1 == 0:
+        print("0 arguments.")
+    else:
+        if n - 1 == 1:
+            print("{} argument:".format(n - 1))
+        else:
+            print("{} arguments:".format(n - 1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
