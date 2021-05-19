f8378f5de5d3a8a5e92492821f0e94c4ad3abf78
lauraliliana <63429954+lauraliliana@users.noreply.github.com>
2021-05-18 20:52:34 -0500

    Add files via upload
    0x06

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Square Module """
+
+
+class Square:
+    """ This is a square class """
+    def __init__(self, size):
+        """ Define size of the square """
+        self.__size = size
